001e11827c64ea07b66d61183227062e92911bd2
Saravana Kumar <saravana.anbarasu@gmail.com>
2025-06-16 12:07:00 +0530

    java 8 examples

---
+++ labs/examples/adv-java-examples/src/com/examples/java/java8/time/CalendarSystemDemo.java
@@ -0,0 +1,37 @@
+package com.examples.java.java8.time;
+
+import java.time.LocalDate;
+import java.time.chrono.*;
+
+public class CalendarSystemDemo {
+
+    public static void main(String[] args) {
+        // Gregorian Calendar
+        LocalDate gregorianDate = LocalDate.now();
+        System.out.println("Gregorian Date: " + gregorianDate);
+
+//        // Buddhist Calendar
+//        LocalDate buddhistDate = LocalDate.now(ThaiBuddhistChronology.INSTANCE);
+//        System.out.println("Buddhist Date: " + buddhistDate);
+//
+//        // Japanese Calendar
+//        LocalDate japaneseDate = LocalDate.now(JapaneseChronology.INSTANCE);
+//        System.out.println("Japanese Date: " + japaneseDate);
+//
+//        // Islamic (Hijri) Calendar
+//        LocalDate hijriDate = LocalDate.now(HijrahChronology.INSTANCE);
+//        System.out.println("Hijri Date: " + hijriDate);
+//
+//        // Persian Calendar
+//        LocalDate persianDate = LocalDate.now(PersianChronology.INSTANCE);
+//        System.out.println("Persian Date: " + persianDate);
+//
+//        // Thai Calendar
+//        LocalDate thaiDate = LocalDate.now(ThaiBuddhistChronology.INSTANCE);
+//        System.out.println("Thai Date: " + thaiDate);
+    }
+}
+
+
+
+
