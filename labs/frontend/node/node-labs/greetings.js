4e7cddd30dc618e09c064c38d155b81c36a22090
Saravana Kumar <saravana.anbarasu@gmail.com>
2025-08-12 16:24:25 +0530

    node labs

---
+++ labs/frontend/node/node-labs/greetings.js
@@ -0,0 +1,11 @@
+
+export function greet(name = 'Guest') {
+    return `Hello, ${name}!`;
+}
+
+// function greet(name = 'Guest') {
+//     return `Hello, ${name}!`;
+// }
+
+// const greetings = { greet };
+// export default greetings;
---
+++ labs/frontend/node/node-labs/main.js
@@ -0,0 +1,10 @@
+import {greet} from './greetings.js';
+
+// console.log("Hello, World!");
+// console.log("This is a Node.js application.");
+// console.log("Running main.js script.");
+// console.log("Make sure to run 'npm start' to execute this script.");
+
+console.log(greet("Alice"));
+console.log(greet("Bob"));
+console.log(greet());
